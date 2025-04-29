95046f9316616d1213c0c185d2c0616b68c45417
Reza Bahtiar <rezaosd339@gmail.com>
2025-04-29 19:26:51 +0800

    verfikasi hanya user yang dapat melakukan update dan delete pada berita yang dibuat sendiri

--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { getAllNews, addNewNews, getNewsByTitle, updateNews, deleteNews } from "../controllers/articleController.js";
 import verifyToken from "../middlewares/authMiddleware.js";
+import verifyIsNewsOwner from "../middlewares/authIsNewsOwner.js";
 
 const routerArticle = express.Router();
 
@@ -14,10 +15,10 @@ routerArticle.post("/", verifyToken, addNewNews); // hanya dapat diakses oleh us
 routerArticle.get("/search", getNewsByTitle); // boleh diakses oleh semua user
 
 // route untuk update artikel
-routerArticle.patch("/:id", verifyToken, updateNews); // hanya dapat diakses oleh user yang telah login
+routerArticle.patch("/:id", verifyToken, verifyIsNewsOwner, updateNews); // hanya dapat diakses oleh user yang telah login
 
 // route untuk hapus artikel
-routerArticle.delete("/:id", verifyToken, deleteNews); // hanya dapat diakses oleh user yang telah login
+routerArticle.delete("/:id", verifyToken, verifyIsNewsOwner, deleteNews); // hanya dapat diakses oleh user yang telah login
 
 export default routerArticle;
 
