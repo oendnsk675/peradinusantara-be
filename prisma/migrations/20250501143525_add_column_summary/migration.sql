5ec3c67d1a3e5390e93328a0bcab467b260f2ee1
sayidina ahmadal qososyi <sayidinaahmadalqososyi@gmail.com>
2025-05-01 23:05:40 +0800

    fix: naming table in migration

--- prisma/migrations/20250501143525_add_column_summary/migration.sql
+++ prisma/migrations/20250501143525_add_column_summary/migration.sql
@@ -1,11 +1,11 @@
 -- DropForeignKey
-ALTER TABLE `news` DROP FOREIGN KEY `News_category_id_fkey`;
+ALTER TABLE `News` DROP FOREIGN KEY `News_category_id_fkey`;
 
 -- DropIndex
-DROP INDEX `News_category_id_fkey` ON `news`;
+DROP INDEX `News_category_id_fkey` ON `News`;
 
 -- AlterTable
-ALTER TABLE `news` ADD COLUMN `summary` TEXT NULL,
+ALTER TABLE `News` ADD COLUMN `summary` TEXT NULL,
     MODIFY `category_id` INTEGER NULL,
     MODIFY `image` VARCHAR(191) NULL;
 
